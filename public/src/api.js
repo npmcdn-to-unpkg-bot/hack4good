a734d9cbf358cdbd40181eb3b2deb36946efa97b
Chris <chrismonstah@googlemail.com>
2015-12-05 15:37:37 +0100

    initial api

---
+++ public/src/api.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+	
+	var host =  "";
+	var port = "";
+	
+	function getSession(id) {
+		
+	};
+	
+	function postMessage(sessionId, message) {
+		
+	}
+	
+	
+	function doRequest() {
+		
+	};
+	
+});
