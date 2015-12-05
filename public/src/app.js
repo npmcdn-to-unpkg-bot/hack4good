fe2bb1e80f2d5d8f0c99c43afaf6597bd1923f40
Kostja <k.gontscharow@gmail.com>
2015-12-05 16:08:47 +0100

    landing page defaults

--- public/src/app.js
+++ public/src/app.js
@@ -17,14 +17,94 @@ var Header = React.createClass({
     }
 });
 
+var LanguageFlag = React.createClass({
+    getDefaultProps: function () {
+        return {
+            baseUrl: 'assets/flags/512/',
+            country: 'Germany'
+        }
+    },
+
+    getFullUrl: function () {
+        return this.props.baseUrl + this.props.country + ".png"
+    },
+
+    render: function () {
+        return <img src={this.getFullUrl()} width="100%"/>;
+    }
+});
+
+var LanguageTable = React.createClass({
+    getDefaultProps: function () {
+        return {
+            speaking: ['Germany', 'United-kingdom'],
+            learning: ['Albania', 'Bosnian', 'Kosovo']
+        };
+    },
+
+    render: function () {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col s6">You speak</div>
+                    <div className="col s6">You want to learn</div>
+                </div>
+                <div className="row">
+                    <div className="col s6"><LanguageFlag country="Germany" /></div>
+                    <div className="col s6"><LanguageFlag country="Albania" /></div>
+                </div>
+            </div>);
+    }
+});
+
 var Content = React.createClass({
 
+    getDefaultProps: function() {
+        return {
+            roles: {
+                helper: 1,
+                refugee: 0
+            },
+            role: null
+        }
+    },
 
+    getInitialState: function () {
+        return {
+            role: this.props.role
+        }
+    },
+
+    printRole: function () {
+        if (this.state.role == this.props.roles.helper) {
+            return 'You\'re a helper';
+        } else if (this.state.role == this.props.roles.refugee) {
+            return 'You\'re a refugee';
+        } else {
+            return "Nothing";
+        }
+    },
+
+    setHelper: function () {
+        console.log("helper");
+        this.setState({role: this.props.helper});
+    },
+
+    setRefugee: function() {
+        console.log("refugee");
+        this.setState({role: this.props.refugee});
+    },
+
+    setLanguages: function () {
+
+    },
 
     render: function() {
         return (
             <div>
-                #CONETENT
+                <h2>Are you a <a href="#" onClick={this.setHelper}>Helper</a> or <a href="#" onClick={this.setRefugee}> Refugee</a></h2>
+
+                <LanguageTable onClick={this.setLanguages}/>
             </div>
         );
     }
