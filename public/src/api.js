e7d7cef3649dbf814ad1383549001976a9a6155d
Chris <chrismonstah@googlemail.com>
2015-12-05 16:07:59 +0100

    doc mock

--- public/src/api.js
+++ public/src/api.js
@@ -1,19 +1,59 @@
 jQuery(document).ready(function() {
-	
-	var host =  "";
+
+	var host = "";
 	var port = "";
-	
+
 	function getSession(id) {
-		
+		// mock
+
+		var sessionObj = {
+
+		};
+
+		return sessionObj;
+
 	};
-	
+
 	function postMessage(sessionId, message) {
-		
-	}
-	
-	
-	function doRequest() {
-		
+
 	};
-	
+
+	function getDocuments(language) {
+		var documents = {
+			'data' : [{
+				'title' : "this is a title",
+				'tags' : [{
+					'lang' : 'de',
+					'name' : 'language',
+					'date' : 1449317640
+				}],
+				'url' : 'http://refugee-board.de/',
+				'date' : 1449317640
+			}, {
+				'title' : "this is another title",
+				'tags' : [{
+					'lang' : 'en',
+					'name' : 'language',
+					'date' : 1449317640
+				}],
+				'url' : 'http://refugee-board.de/',
+				'date' : 1449317640
+			},{
+				'title' : "this is a title",
+				'tags' : [{
+					'lang' : 'de',
+					'name' : 'language',
+					'date' : 1449317640
+				}],
+				'url' : 'http://refugee-board.de/',
+				'date' : 1449317640
+			}]
+		};
+		return documents;
+	};
+
+	function doRequest(method, url, postData) {
+
+	};
+
 });
