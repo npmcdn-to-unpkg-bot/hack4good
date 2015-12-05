437af5f825cf5057b112cddaabea5b2d17d7f861
dadarakt <janniseichborn@gmail.com>
2015-12-05 15:16:03 +0100

    First DB prototype

--- hack2godly/build.sbt
+++ hack2godly/build.sbt
@@ -12,8 +12,10 @@ libraryDependencies ++= Seq(
   "net.databinder" %% "unfiltered-util" % "0.8.4",
   "com.quantifind" %% "sumac" % "0.3.0",
   "com.typesafe" % "config" % "1.3.0",
-  "com.twitter" %% "util-collection" % "6.27.0"
-
+  "com.twitter" %% "util-collection" % "6.27.0",
+  "com.typesafe.slick" %% "slick" % "3.1.0",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "com.h2database" % "h2" % "1.3.166"
 )
 
 
--- hack2godly/src/main/scala/server/Main.scala
+++ hack2godly/src/main/scala/server/Main.scala
@@ -1,8 +1,16 @@
 package server
 
+import database.{Document, DocumentTable}
 import server.WebApp.Arguments
 import unfiltered.response.ResponseString
 import unfiltered.request._
+
+import scala.concurrent.Future
+import scala.util.{Success, Failure}
+
+//import slick.driver.MySQLDriver.api._
+import slick.driver.H2Driver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by jannis on 12/5/15.
@@ -10,6 +18,31 @@ import unfiltered.request._
 object Main extends WebApp[Args] {
 
   class ServerApp(args: Arguments) extends unfiltered.filter.Plan {
+
+    // the database object for interactions
+    val db = Database.forConfig("documentdb")
+    val docs = DocumentTable.props
+    val setup = DBIO.seq(
+      docs.schema.create,
+      docs += Document(0, "content", "owner", "tags")
+    )
+
+    val setupFuture = db.run(setup)
+    setupFuture.onComplete {
+      case Failure(ex) => println(s"Could not setup db because of, $ex")
+      case Success(_) => println("Successfully setup database")
+    }
+
+    println("test output from the database")
+    val q = for (d <- docs) yield d.content
+    val a = q.result
+    val f: Future[Seq[String]] = db.run(a)
+
+    f.onComplete {
+      case Success(r) => println(r)
+      case Failure(ex) => println(s"Couldn't recieve data because of $ex")
+    }
+
     def intent = {
       case req@GET(Path(Seg("test" :: Nil)) & Params(params)) =>
         ResponseString("Das hier ist ein simpler test")
